4044dba9a575dadd111627376ad401946c1e0a21
timdereuse <timdereuse@gmail.com>
2021-06-03 15:56:33 -0500

    developing patterns to use clique-based approach

--- find_developing_patterns.py
+++ find_developing_patterns.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 from fractions import Fraction
 from pathlib import Path
 from graph import Graph
+from fastdtw import fastdtw
 from numpy.lib.stride_tricks import as_strided
 
 import visualize_motifs as vm
@@ -15,17 +16,19 @@ reload(vm)
 reload(gv)
 reload(pattern)
 
-similarity_thresh_scale = 5.2
-cardinalities = [4, 5, 6]
+cardinalities = [4, 5, 6, 7]
 min_occurrences = 4
+prop_edges_to_keep = 0.05
 num_paths_to_check = 200
+seq_compare_dist_threshold = 8
 tune_name = 'meshigene'
-
 
 us = m21.environment.UserSettings()
 # us['musescoreDirectPNGPath'] = Path(r"C:\Program Files\MuseScore 3\bin\MuseScore3.exe")
 
-fname = r'Meshigene - transcription sax solo.musicxml'
+# fname = r'Meshigene - transcription sax solo.musicxml'
+fname = r'Falling Grace solo.musicxml'
+
 mus_xml = m21.converter.parse(fname)
 x = list(mus_xml.recurse().getElementsByClass('PageLayout'))
 mus_xml.remove(x, recurse=True)
@@ -39,7 +42,6 @@ num_events = len(main_feat_seq)
 note_self_similarity = np.matmul(feats_arr, feats_arr.T)
 note_self_similarity[np.identity(num_events) == 1] = 0
 
-similarity_thresh = np.mean(note_self_similarity) * similarity_thresh_scale
 
 def get_subsequences(arr, m):
     n = arr.size - m + 1
@@ -47,61 +49,64 @@ def get_subsequences(arr, m):
     return as_strided(arr, shape=(m,n), strides=(s,s))
 
 
-def get_similarity(sequence_a, sequence_b, note_self_similarity):
-
-    res = 0
-
-    # return 0 if these subsequences overlap
-    if not (set(sequence_a).intersection(set(sequence_b)) == set()):
-        return 0
-
-    # return 0 if sufficiently different in length
-    if np.abs(len(sequence_a) - len(sequence_b)) > 1:
-        return 0
-    elif np.abs(len(sequence_a) - len(sequence_b)) == 1:
-        longer_seq = max(sequence_a, sequence_b, key=len)
-        shorter_seq = min(sequence_a, sequence_b, key=len)
-        # exclude each element of the longer sequence in turn
-
-        scores = []
-        for i in range(len(longer_seq)):
-            ss = get_similarity(shorter_seq, np.delete(longer_seq, i), note_self_similarity)
-            scores.append(ss)
-
-        correction_term = len(shorter_seq) / len(longer_seq)
-        return max(scores) * correction_term
-
-    for i in range(len(sequence_a)):
-        idx_a = sequence_a[i]
-        idx_b = sequence_b[i]
-        res += note_self_similarity[idx_a, idx_b]
-    res = res / len(sequence_a)
-    return res
+def get_similarity(sequence_a, sequence_b, feats_arr):
+    sq_a = feats_arr[sequence_a]
+    sq_b = feats_arr[sequence_b]
+    distance, path = fastdtw(sq_a, sq_b)
+    distance = distance / (min(len(sq_a), len(sq_b)))
+    return distance
 
 
-print('building graph...')
 sqs = [list(get_subsequences(np.arange(num_events), num_events - (i - 1))) for i in cardinalities]
 sqs = [item for sublist in sqs for item in sublist]
 sqs = sorted(sqs, key=lambda x: tuple(x))
 
-g = Graph()
 
+# make list of pairs of sequences that will be compared.
+print('getting list of candidate pairs of sequences...')
+pairs_to_compare = []
+for i, sqa in enumerate(sqs):
+    for j, sqb in enumerate(sqs[i:]):
+        # do not evaluate overlapping sequences
+        if not (set(sqa).intersection(set(sqb)) == set()):
+            continue
+        # evaluate only sequences that are close together
+        dist_1 = np.abs(max(sqa) - min(sqb))
+        dist_2 = np.abs(max(sqb) - min(sqa))
+        if not (dist_1 < seq_compare_dist_threshold or dist_2 < seq_compare_dist_threshold):
+            continue
+        pairs_to_compare.append((i, j + i))
+
+
+
+# compute all similarity scores for selected pairs of sequences
+print('computing similarity scores for all pairs of sequences...')
+scores = []
+for n, sq_pair in enumerate(pairs_to_compare):
+    i, j = sq_pair
+    similarity = get_similarity(sqs[i], sqs[j], feats_arr)
+    scores.append(similarity)
+
+    if not n % (len(pairs_to_compare) // 10):
+        print(f'   {n} / {len(pairs_to_compare)} scores calculated...')
+
+# get threshold
+dist_thresh = sorted(scores)[int(prop_edges_to_keep * len(scores))]
+
+print('building graph...')
+g = Graph()
 for i, sq in enumerate(sqs):
     g.add_node(i)
 
-    for n in g.nodes():
-        if i == n:
-            continue
+for i, sq_pair in enumerate(pairs_to_compare):
+    score = scores[i]
+    if score < dist_thresh:
+        g.add_edge(sq_pair[0], sq_pair[1], score)
+        # g.add_edge(j, i, score)
 
-        similarity = get_similarity(sqs[i], sqs[n], note_self_similarity)
-
-        if similarity > similarity_thresh:
-            g.add_edge(n, i, -1 * similarity)
-            # print(i, n, similarity)
-
-    if not i % (len(sqs) // 20):
-        print(f'   {i} of {len(sqs)} nodes... \n'
-              f'   {len(g.edges())} edges added')
+    if not i % (len(pairs_to_compare) // 10):
+        print(f'   {i} / {len(pairs_to_compare)} nodes... \n'
+              f'   {len(g.edges())} total edges added')
 
 orphaned_nodes = set(g.nodes(out_degree=0)).intersection(g.nodes(in_degree=0))
 print(f'removing {len(orphaned_nodes)} orphaned nodes from graph.')
@@ -114,7 +119,7 @@ all_paths = g.all_pairs_shortest_paths()
 duples = {}
 for x in all_paths.keys():
     for y in all_paths[x].keys():
-        if all_paths[x][y] == np.inf:
+        if x == y or all_paths[x][y] == np.inf:
             continue
         duples[(x, y)] = all_paths[x][y]
 
@@ -123,6 +128,8 @@ x = sorted(duples.keys(), key=lambda x: duples[x])
 good_paths = []
 for i in range(num_paths_to_check):
     p = g.shortest_path(x[i][0], x[i][1], memoize=True)
+
+    print(p)
 
     if len(p[1]) < min_occurrences:
         continue
@@ -154,7 +161,27 @@ for i, path in enumerate(good_paths):
                 f.write(f'    Note {idx}: {vps} \n')
 
 
+def plot_xy_graph(g):
+    from graph.random import random_xy_graph
+    from graph.hash import graph_hash
+    from graph.visuals import plot_2d
 
+    node_count = len(g.nodes())
+    xygraph = Graph()
 
+    for i, node in enumerate(g.nodes()):
+        x = (i * 12)
+        y = np.random.randint(0, node_count * 2)
+        xygraph.add_node((x, y))
+        
+
+    mapping = {b:a for a,b in zip(xygraph.nodes(), g.nodes())}
+
+    for edge in g.edges():
+        start, end, distance = edge
+        xygraph.add_edge(mapping[start], mapping[end], distance)
+    plt = plot_2d(xygraph)
+    plt.show()
 
 # mus_xml.write('musicxml.pdf', fp='./exports/testexport')
+
